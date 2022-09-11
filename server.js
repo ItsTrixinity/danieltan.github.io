81a31555fba41c69ce079f32da03c2792e958df7
jtan0189 <jtan0189@student.monash.edu>
2022-09-12 03:06:20 +0800

    Modified Server Backend, Enabled Form Submission

--- server.js
+++ server.js
@@ -2,51 +2,56 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+require('dotenv').config();
+let PORT = process.env.PORT || 5000;
 
-// server used to send send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
+app.listen(PORT, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: "********@gmail.com",
-    pass: ""
-  },
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth:{
+        type: "OAuth2",
+        user: process.env.REACT_APP_EMAIL,
+        clientId: process.env.REACT_APP_CLIENT_ID,
+        clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+        refreshToken: process.env.REACT_APP_REFRESH_TOKEN,
+        accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+    },
 });
 
 contactEmail.verify((error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Ready to Send");
-  }
+    if(error){
+        console.log(error);
+    }else {
+        console.log("Ready to Send");
+    }
 });
 
 router.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
-  const email = req.body.email;
-  const message = req.body.message;
-  const phone = req.body.phone;
-  const mail = {
-    from: name,
-    to: "********@gmail.com",
-    subject: "Contact Form Submission - Portfolio",
-    html: `<p>Name: ${name}</p>
-           <p>Email: ${email}</p>
-           <p>Phone: ${phone}</p>
-           <p>Message: ${message}</p>`,
-  };
-  contactEmail.sendMail(mail, (error) => {
-    if (error) {
-      res.json(error);
-    } else {
-      res.json({ code: 200, status: "Message Sent" });
-    }
-  });
-});
+    const name = req.body.firstName + req.body.lastName;
+    const email = req.body.email;
+    const message = req.body.message;
+    const subject = req.body.title;
+    const mail = {
+        from: "daniffyx01@gmail.com",
+        to: "daniffyx01@gmail.com",
+        subject: subject,
+        html: `<p>Name: ${name}</p>
+               <p>Email: ${email}</p>
+               <p>Subject: ${subject}</p>
+               <p>Message: ${message}</p>`,
+    };
+    contactEmail.sendMail(mail, (error) => {
+        if (error) {
+            res.json(error);
+        } else {
+            res.json({code: 200, status: "Message Sent"});
+        }
+    });
+})
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,7 +10,7 @@ export const Contact = () => {
     firstName: '',
     lastName: '',
     email: '',
-    phone: '',
+    title: '',
     message: ''
   }
   const [formDetails, setFormDetails] = useState(formInitialDetails);
@@ -24,6 +24,7 @@ export const Contact = () => {
       })
   }
 
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
@@ -33,14 +34,14 @@ export const Contact = () => {
         "Content-Type": "application/json;charset=utf-8",
       },
       body: JSON.stringify(formDetails),
-    });
+    }).catch(err => {console.log(err)});
     setButtonText("Send");
     let result = await response.json();
     setFormDetails(formInitialDetails);
-    if (result.code == 200) {
-      setStatus({ succes: true, message: 'Message sent successfully'});
+    if (result.code === 200) {
+      setStatus({ success: true, message: 'Your message has been successfully sent!'});
     } else {
-      setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
+      setStatus({ success: false, message: 'Something went wrong. Please try again later!'});
     }
   };
 
@@ -63,27 +64,28 @@ export const Contact = () => {
                 <form onSubmit={handleSubmit}>
                   <Row>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                      <input type="text" value={formDetails.firstName} placeholder="First Name" required onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                     </Col>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
+                      <input type="text" value={formDetails.lastName} placeholder="Last Name" required onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
                     </Col>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                      <input type="email" value={formDetails.email} placeholder="Email Address" required onChange={(e) => onFormUpdate('email', e.target.value)} />
                     </Col>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.phone} placeholder="Title or Subject" onChange={(e) => onFormUpdate('title', e.target.value)}/>
+                      <input type="text" value={formDetails.title} placeholder="Title or Subject" required onChange={(e) => onFormUpdate('title', e.target.value)}/>
                     </Col>
                     <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                      <textarea rows="6" value={formDetails.message} placeholder="Message" required onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                      <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
                       <button type="submit"><span>{buttonText}</span></button>
                     </Col>
-                    {
+                    {/* {
                       status.message &&
                       <Col>
                         <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
                       </Col>
-                    }
+                    } */}
                   </Row>
                 </form>
               </div>}
