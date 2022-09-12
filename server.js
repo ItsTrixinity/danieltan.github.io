41bad7a98b30fd6fce33ee50b5bee744f0b50168
jtan0189 <jtan0189@student.monash.edu>
2022-09-12 19:35:27 +0800

    Reverted package switch.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 require('dotenv').config();
 let PORT = process.env.PORT || 5000;
-// test
+
 const app = express();
 app.use(cors());
 app.use(express.json());
